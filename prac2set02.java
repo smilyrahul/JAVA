bcb69a8f797358950c8a7fd16f79c2fb3ce9b93c
Rahul raj <rahulraj12777@gmail.com>
2023-09-01 16:50:59 +0530

    practice2 set 2

---
+++ prac2set02.java
@@ -0,0 +1,13 @@
+public class prac2set02 {
+    public static void main(String[] args) {
+        char grade ='b';
+        //increment with grade 
+       grade= (char) (grade + 8);
+         System.out.println(grade);
+
+        //decrement with grade
+        grade = (char) (grade - 8);
+        System.out.println(grade);
+    }
+    
+}
