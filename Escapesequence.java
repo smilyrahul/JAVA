87606d987746896efcd7b411a98df5e39078e0a4
Rahul raj <rahulraj12777@gmail.com>
2023-09-02 17:09:22 +0530

    Escapesequence

---
+++ Escapesequence.java
@@ -0,0 +1,30 @@
+public class Escapesequence {
+    public static void main(String[] args) {
+        System.out.println("i am Escape seqence \n your are going right way");
+                System.out.println("i am Escape seqence \' your are going right way");
+        System.out.println("i am Escape seqence \t your are going right way");
+        System.out.println("i am Escape seqence \\ your are going right way");
+        System.out.println("i am Escape seqence \t/our are going right way");
+        
+    }
+}
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
