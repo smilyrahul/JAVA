0e080628a02fe193fd3ee6cdf42d2c2c552ae878
Rahul raj <rahulraj12777@gmail.com>
2023-09-04 13:59:01 +0530

    prac2set3

---
+++ prac2set3.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+public class prac2set3 {
+    public static void main(String[] args) {
+    Scanner sc =new Scanner(System.in);
+        System.out.println("enter a number");
+        int a = sc.nextInt();
+        System.out.println(a>8);
+        sc.close();
+    }
+    
+}
