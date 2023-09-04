17d975c603b28f313947baed71624109e34ec3d3
Rahul raj <rahulraj12777@gmail.com>
2023-09-04 14:00:16 +0530

    prac2set04

---
+++ prac2set04.java
@@ -0,0 +1,11 @@
+public class prac2set04 {
+    public static void main(String[] args) {
+        int x=7;
+        int a=(7*49/7) + (35/7);
+        //float a= (7.0f/49.0f) +(35.0f/7.0f) ;
+        System.out.println(x);
+        System.out.println(a);
+    }
+    
+    
+}
