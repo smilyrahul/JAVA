343161bcce5c0593ec42e4a9bd83626f1a5aa519
Rahul raj <rahulraj12777@gmail.com>
2023-09-04 14:01:27 +0530

    prac2set05

---
+++ prac2set05.java
@@ -0,0 +1,10 @@
+public class prac2set05 {
+    public static void main(String[] args) {
+        int v =8;
+        int u =5;
+        int a = 3;
+        int s = 4;
+        int x=(v*v)-(u*u)/(2*a*s);
+System.out.println(x);
+    }
+}
