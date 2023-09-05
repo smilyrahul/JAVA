b1ddb18575d46f57dd14e7c16b5a0750535caa77
Rahul raj <rahulraj12777@gmail.com>
2023-09-06 03:16:38 +0530

    operator1

---
+++ operator1.java
@@ -0,0 +1,15 @@
+public class operator1 {
+    public static void main(String[] args) {
+        int a=6;
+        // int b=55 + a;
+        int b=5;
+        b += 3;
+        System.out.println(a);
+        System.out.println(b);
+        //System.out.println(6==a);
+         System.out.println(6==9); 
+         System.out.println(64>6 && 64>9); //both condtion should be true otherwise it's false
+         System.out.println(64>6 && 64<9);
+         System.out.println(64>6 );//any condition is true then it's true 
+    }
+}
