22f29ffada5a915f4b44985f5d7ce2251277250b
Rahul raj <rahulraj12777@gmail.com>
2023-09-06 03:12:31 +0530

    pracques

---
+++ pracques.java
@@ -0,0 +1,14 @@
+public class pracques {
+    public static void main(String[] args) {
+        int x=8;
+        int y=1;
+        int k =x*y/2;
+        System.out.println(k);
+        int a=4;
+        int b=4;
+        int c =4;
+        int g =b*b-(4*a*c) /(2*a);
+        System.out.println(g);
+    }
+    
+}
